663be236b432cc72f473d6e214200af0c5340e21
Ravi Van der salm <ravan-de@f1r5s4.codam.nl>
2019-06-26 20:50:16 +0200

    fetched ft_init_board

--- ft_initialize_board.c
+++ ft_initialize_board.c
@@ -6,22 +6,53 @@
 /*   By: dsaripap <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/06/11 19:14:21 by dsaripap      #+#    #+#                 */
-/*   Updated: 2019/06/26 20:28:05 by ravan-de      ########   odam.nl         */
+/*   Updated: 2019/06/11 19:14:25 by dsaripap      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void				initialize_board(unsigned short **board)
+void				initialize_board(uint16_t **board)
 {
 	size_t			i;
 
 	i = 0;
-	*board = (unsigned short *)malloc(sizeof(unsigned short) * 16);
+	*board = (uint16_t *)malloc(sizeof(uint16_t) * 16);
 	while (i < 16)
 	{
-		(*board)[i] = (unsigned short)malloc(sizeof(unsigned short) * 1);
+		(*board)[i] = (uint16_t)malloc(sizeof(uint16_t) * 1);
 		(*board)[i] = 0;
+		//printf("board[%lu] = %d \n", i, board[i]);
 		i++;
 	}
+}
+
+int					square_root(int num)
+{
+	int				temp;
+	int				res;
+
+	temp = 0;
+	res = 0;
+	while (res <= num)
+	{
+		res = temp * temp;
+		//printf("\n temp = %d", temp);
+		if (res == num)
+			return (temp);
+		temp++;
+	}
+	temp--;
+	return (temp);
+}
+
+int					initial_boardsize(t_list *tetr_lst)
+{
+	int				size;
+
+	size = ft_lstlen(tetr_lst);
+	//printf("\n no of tet = %d", size);
+	size = square_root(size * 4);
+	//printf("\n size = %d", size);
+	return (size);
 }
